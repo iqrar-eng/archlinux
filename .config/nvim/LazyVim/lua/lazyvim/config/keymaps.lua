da5b5890028793d97b0bbb37408cde789226ffa1
Iqrar <iqrar.h8197@gmail.com>
2026-09-13 10:42:31 +0500

    add files/dirs

--- .config/nvim/LazyVim/lua/lazyvim/config/keymaps.lua
+++ .config/nvim/LazyVim/lua/lazyvim/config/keymaps.lua
@@ -528,14 +528,14 @@ end, { desc = "Edit or Create file in current dir" })
 ----------------------------------------------
 
 vim.keymap.set("n", "<leader>ja", function()
-  vim.cmd("Git add %")
+  vim.cmd("silent! Git add %")
 end, { desc = "Git add %" })
 
 vim.keymap.set("n", "<leader>jA", function()
-  vim.cmd("Git add -A")
-  vim.cmd("Git commit -m 'add files/dirs'")
+  vim.cmd("silent! Git add -A")
+  vim.cmd("silent! Git commit -m 'add files/dirs'")
 end, { desc = "Git add, commit" })
 
 vim.keymap.set("n", "<leader>jp", function()
-  vim.cmd("Git push")
+  vim.cmd("silent! Git push")
 end, { desc = "Git push" })
